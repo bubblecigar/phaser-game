202ad1371052ae017280f813cec80b846ef6e0d0
王瑞瑜 <roywang@h2o-team.com>
2021-06-24 18:28:20 +0800

    lint addPlayer() and remove circle mask

--- share/game.ts
+++ share/game.ts
@@ -93,12 +93,7 @@ const gameMethods = (env: 'client' | 'server') => variables => {
 
         if (playerConstructor.id === variables.userId) {
           const camera = scene.cameras.cameras[0]
-          camera.startFollow(player.phaserObject, false, 0.2, 0.2)
-          camera.roundPixels = true
-          const Phaser = variables.Phaser
-          const circle = new Phaser.GameObjects.Graphics(scene).fillCircle(gameConfig.canvasWidth / 2, gameConfig.canvasHeight / 2, 230)
-          const mask = new Phaser.Display.Masks.GeometryMask(scene, circle)
-          camera.setMask(mask)
+          camera.startFollow(player.phaserObject, true, 1, 1)
           const wallLayer = scene.children.getByName('wall_layer')
           scene.physics.add.collider(player.phaserObject, wallLayer)
         }
