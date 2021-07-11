1063925ea44d2d95fdbb237c2b1aa5dc7e69b834
王瑞瑜 <roywang@h2o-team.com>
2021-07-11 23:16:46 +0800

    change userInteraction to playerInteraction

--- client/js/scene/WorldEvents.ts
+++ client/js/scene/WorldEvents.ts
@@ -1,6 +1,6 @@
 import { getLocalUserData } from '../user'
 
-const userInteraction = (scene, userBody, userData, targetBody, targetData) => {
+const playerInteraction = (scene, userBody, userData, targetBody, targetData) => {
   const isUserSensor = userBody.label === 'body-sensor'
   const triggerIndoorSensor = targetData.interface === 'fov-sensor'
   if (isUserSensor && triggerIndoorSensor) {
@@ -20,16 +20,15 @@ const registerWorlEvents = scene => {
     if (!dataA || !dataB) {
       return
     }
-    const user = getLocalUserData()
-    const userIsA = dataA.id === user.userId
-    const userIsB = dataB.id === user.userId
-    const userInvolved = userIsA || userIsB
-    if (userInvolved) {
-      const userBody = userIsA ? bodyA : bodyB
-      const userData = userIsA ? dataA : dataB
-      const targetBody = userIsA ? bodyB : bodyA
-      const targetData = userIsA ? dataB : dataA
-      userInteraction(scene, userBody, userData, targetBody, targetData)
+    const playerIsA = dataA.interface === 'Player'
+    const playerIsB = dataB.interface === 'Player'
+    const playerInvolved = playerIsA || playerIsB
+    if (playerInvolved) {
+      const userBody = playerIsA ? bodyA : bodyB
+      const userData = playerIsA ? dataA : dataB
+      const targetBody = playerIsA ? bodyB : bodyA
+      const targetData = playerIsA ? dataB : dataA
+      playerInteraction(scene, userBody, userData, targetBody, targetData)
     }
   })
   scene.matter.world.on('collisionend', function (event, bodyA, bodyB) {
@@ -47,7 +46,7 @@ const registerWorlEvents = scene => {
       const userData = userIsA ? dataA : dataB
       const targetBody = userIsA ? bodyB : bodyA
       const targetData = userIsA ? dataB : dataA
-      userInteraction(scene, userBody, userData, targetBody, targetData)
+      playerInteraction(scene, userBody, userData, targetBody, targetData)
     }
   })
 }
