48f4671fcc60505293e27ba8c296a0d35f74f728
王瑞瑜 <roywang@h2o-team.com>
2021-09-15 17:22:37 +0800

    clean up GUI

--- client/js/scene/dungeon/GUI.ts
+++ client/js/scene/dungeon/GUI.ts
@@ -4,20 +4,10 @@ import gameState from '../../game/state'
 import gameConfig from '../../game/config'
 import { getLocalUserData } from '../../user'
 import items from '../../items/index'
-// import { aimingTime, skillInUse } from '../../skills/index'
 
-let scene, coinGroup, maximumBar, currentBar
-let aimingBarContainer, aimingBar
-const abilityBar = {
-  dmg: null,
-  spd: null,
-  dir: null,
-  dur: null,
-  wave: null
-}
+let coinGroup
 
 function preload() {
-  scene = this
   this.load.image('item_cell', itemCellUrl)
   Object.keys(items).forEach(
     key => {
@@ -26,15 +16,6 @@ function preload() {
     }
   )
 }
-
-// const addItemCell = (x, y, i) => {
-//   const itemCell = scene.add.image(x, y, 'item_cell')
-//   itemCell.setX(itemCell.x + (itemCell.width - 1) * i)
-//   itemCell.angle = 90 * (i + 1)
-//   const text = scene.add.text(itemCell.x + 3, y + 3, i + 1, { fontSize: 8 })
-//   text.setOrigin(0, 0)
-//   return itemCell
-// }
 
 function create() {
   Object.keys(items).forEach(
@@ -55,77 +36,11 @@ function create() {
   )
 
   const padding = 32
-  // const itemX = gameConfig.canvasWidth / 2 - 16
-  // const itemY = gameConfig.canvasHeight - padding
-  // addItemCell(itemX, itemY, 0)
-  // addItemCell(itemX, itemY, 1)
-  // addItemCell(itemX, itemY, 2)
 
   const coinX = padding
   const coinY = gameConfig.canvasHeight - padding
   createCoinGroup(this, coinX, coinY)
-
-  // const aimingBarX = gameConfig.canvasWidth / 2 - 10
-  // const aimingBarY = gameConfig.canvasHeight / 2 + 20
-  // createAimingBar(aimingBarX, aimingBarY)
-
-  // const abilityX = padding
-  // const abilityY = gameConfig.canvasHeight - padding
-  // createAbilityBar(abilityX, abilityY - 30, 'dmg', 1 / 9)
-  // createAbilityBar(abilityX, abilityY - 20, 'spd', 1 / 9)
-  // createAbilityBar(abilityX, abilityY - 10, 'dur', 1 / 9)
-  // createAbilityBar(abilityX, abilityY, 'dir', 1 / 9)
-  // createAbilityBar(abilityX, abilityY + 10, 'wave', 1 / 9)
 }
-
-const createAbilityBar = (x, y, label, percentage) => {
-  const barLength = 40
-  const text = scene.add.text(x - 3, y, label, { fontSize: '8px' })
-  text.setOrigin(1, 0.5)
-  const maximumBar = scene.add.rectangle(x - 1, y, barLength, 6, 0xDDDDDD)
-  maximumBar.setOrigin(0, 0.5)
-  const currentBar = scene.add.rectangle(x, y, percentage * barLength - 2, 4, 0x4ba747)
-  currentBar.setOrigin(0, 0.5)
-  abilityBar[label] = currentBar
-}
-
-const showAbilityBar = (abilities) => {
-  const barLength = 40
-  abilityBar.dmg.setSize(barLength * abilities.damageMultiplier / 9 - 2, 4)
-  abilityBar.dmg.setOrigin(0, 0.5)
-  abilityBar.spd.setSize(barLength * abilities.speedMultiplier / 9 - 2, 4)
-  abilityBar.spd.setOrigin(0, 0.5)
-  abilityBar.dur.setSize(barLength * abilities.durationMultiplier / 9 - 2, 4)
-  abilityBar.dur.setOrigin(0, 0.5)
-  abilityBar.dir.setSize(barLength * abilities.directions.length / 9 - 2, 4)
-  abilityBar.dir.setOrigin(0, 0.5)
-  abilityBar.wave.setSize(barLength * abilities.consectiveShooting / 9 - 2, 4)
-  abilityBar.wave.setOrigin(0, 0.5)
-}
-
-// const createAimingBar = (x, y) => {
-//   aimingBarContainer = scene.add.rectangle(x - 1, y, 20 + 2, 5, 0xFFFFFF)
-//   aimingBarContainer.setOrigin(0, 0.5)
-//   aimingBar = scene.add.rectangle(x, y, aimingTime, 3)
-// }
-
-// const showAimingBar = () => {
-//   if (!aimingTime || !skillInUse) {
-//     aimingBar.setVisible(false)
-//     aimingBarContainer.setVisible(false)
-//   } else {
-//     aimingBarContainer.setVisible(true)
-//     aimingBar.setVisible(true)
-//     const percentage = Math.min(aimingTime / skillInUse.castTime, 1)
-//     aimingBar.setSize(percentage * 20, 3)
-//     aimingBarContainer.setOrigin(0, 0.5)
-//     if (percentage >= 1) {
-//       aimingBar.setFillStyle(0x4ba747)
-//     } else {
-//       aimingBar.setFillStyle(0xee8e2e)
-//     }
-//   }
-// }
 
 const createCoinGroup = (scene, x, y) => {
   coinGroup = scene.add.group({ classType: Phaser.GameObjects.Sprite })
@@ -141,12 +56,10 @@ const showCoinCount = count => {
   coinGroup.setVisible(false, count, 1)
 }
 
-function update(t, dt) {
+function update() {
   const player = gameState.players.find(p => p.id === getLocalUserData().userId)
   if (!player) return
   showCoinCount(player.coins)
-  // showAimingBar()
-  // showAbilityBar(player.abilities)
 }
 
 export default {
