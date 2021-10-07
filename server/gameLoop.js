de081fc0b021dee27a4613ab2e44e654c8cc2be4
王瑞瑜 <roywang@h2o-team.com>
2021-10-07 16:15:34 +0800

    refactor gameLoop.js from roomMethods.js

---
+++ server/gameLoop.js
@@ -0,0 +1,131 @@
+const uuid = require('uuid')
+const setting = require('../share/setting.json')
+const serverMap = require('../share/serverMap.json')
+const rooms = require('./rooms').rooms
+
+const intervalTimeStep = 200
+
+const closeRoom = (roomId) => {
+  const room = rooms[roomId]
+  Object.keys(room.intervals).forEach(
+    prop => {
+      room.intervals[prop].forEach(
+        interval => {
+          clearInterval(interval)
+        }
+      )
+    }
+  )
+  delete rooms[roomId]
+}
+
+
+const registerRoomAutoCloseInterval = room => setInterval(
+  () => {
+    if (room.players.length > 0) {
+      // room in use
+      room.idleTime = 0
+    } else {
+      // room in idle
+      if (room.idleTime >= setting.roomAutoCloseIdleTime) {
+        closeRoom(roomId)
+      }
+      room.idleTime += intervalTimeStep
+    }
+  }, intervalTimeStep
+)
+
+const registerWaitingIntervals = room => setInterval(
+  () => {
+    const allPlayerReady = !room.players.some(player => !player.ready)
+    const enoughPlayers = room.players.length >= setting.minumumPlayers
+    const readyForEnoughTime = room.allPlayerReadyTime >= setting.gameStartCountDown
+    if (enoughPlayers && allPlayerReady) {
+      if (readyForEnoughTime) {
+        room.allPlayerReadyTime = 0
+        changeGameStatus(room, 'processing')
+      } else {
+        room.allPlayerReadyTime += intervalTimeStep
+      }
+    } else {
+      room.allPlayerReadyTime = 0
+    }
+  }, intervalTimeStep
+)
+
+const createCoin = () => {
+  const mapFile = serverMap.processing.map
+  const mapUrl = `../share/map/${mapFile}`
+  const map = require(mapUrl)
+  const infoLayer = map.layers.find(layer => layer.name === 'info_layer')
+  const coinSpawnPoints = infoLayer.objects.filter(object => object.name === 'coin_point')
+  const randomCoinSpawnIndex = Math.floor(Math.random() * (coinSpawnPoints.length))
+  const coinSpawnPoint = coinSpawnPoints[randomCoinSpawnIndex]
+  const coinConstructor = {
+    interface: 'Item',
+    id: uuid(),
+    itemKey: 'coin',
+    position: { x: coinSpawnPoint.x, y: coinSpawnPoint.y },
+    velocity: { x: 0, y: 0 },
+    phaserObject: null
+  }
+  return coinConstructor
+}
+
+const registerProcessingIntervals = room => setInterval(
+  () => {
+    const coins = room.items.filter(item => item.itemKey === 'coin')
+    if (coins.length > 0) {
+      // do nothing
+    } else {
+      const coinConstructor = createCoin()
+      room.items.push(coinConstructor)
+      const { io } = require('./index.js')
+      io.in(room.id).emit('dungeon', 'addItem', coinConstructor)
+    }
+
+    // check end game condition
+    // -> emit game end event
+    // -> change game status to ending
+  }, intervalTimeStep
+)
+
+const registerEndingIntervals = room => setInterval(
+  () => {
+    // generate end game report
+    // -> emit end game report to clients
+    // -> setTimeout and cycle gameStatus to waiting
+  }, intervalTimeStep
+)
+
+const changeGameStatus = (room, newGameStatus) => {
+  const gameStatusIntervals = room.intervals.byGameStatus
+  gameStatusIntervals.forEach(interval => clearInterval(interval))
+  gameStatusIntervals.splice(0, gameStatusIntervals.length)
+
+  if (newGameStatus === 'waiting') {
+    gameStatusIntervals.push(registerWaitingIntervals(room))
+  } else if (newGameStatus === 'processing') {
+    gameStatusIntervals.push(registerProcessingIntervals(room))
+  } else if (newGameStatus === 'ending') {
+    gameStatusIntervals.push(registerEndingIntervals(room))
+  } else {
+    // wrong status, throw
+  }
+
+  room.gameStatus = newGameStatus
+  const { io } = require('./index.js')
+  io.to(room.id).emit('game', 'updateGameStatus', room.gameStatus)
+}
+
+const registerGameLoop = room => {
+  room.intervals = {
+    alltime: [registerRoomAutoCloseInterval(room)],
+    byGameStatus: [registerWaitingIntervals(room)]
+  }
+}
+
+
+exports.gameLoop = {
+  registerGameLoop
+}
--- server/roomMethods.js
+++ server/roomMethods.js
@@ -1,38 +1,7 @@
-const uuid = require('uuid')
 const setting = require('../share/setting.json')
 const serverMap = require('../share/serverMap.json')
 const rooms = require('./rooms').rooms
-
-const intervalTimeStep = 200
-
-const closeRoom = (roomId) => {
-  const room = rooms[roomId]
-  Object.keys(room.intervals).forEach(
-    prop => {
-      room.intervals[prop].forEach(
-        interval => {
-          clearInterval(interval)
-        }
-      )
-    }
-  )
-  delete rooms[roomId]
-}
-
-const registerRoomAutoCloseInterval = room => setInterval(
-  () => {
-    if (room.players.length > 0) {
-      // room in use
-      room.idleTime = 0
-    } else {
-      // room in idle
-      if (room.idleTime >= setting.roomAutoCloseIdleTime) {
-        closeRoom(roomId)
-      }
-      room.idleTime += intervalTimeStep
-    }
-  }, intervalTimeStep
-)
+const gameLoop = require('./gameLoop').gameLoop
 
 const createRoom = (roomId) => {
   const room = {
@@ -46,10 +15,9 @@ const createRoom = (roomId) => {
     intervals: null,
     methods: null
   }
-  room.intervals = {
-    alltime: [registerRoomAutoCloseInterval(room)],
-    byGameStatus: [registerWaitingIntervals(room)]
-  }
+
+  gameLoop.registerGameLoop(room)
+
   room.methods = {
     writePlayer: (playerState) => {
       const playerIndex = room.players.findIndex(player => player.id === playerState.id)
@@ -75,89 +43,6 @@ const createRoom = (roomId) => {
   }
 
   return room
-}
-
-const registerWaitingIntervals = room => setInterval(
-  () => {
-    const allPlayerReady = !room.players.some(player => !player.ready)
-    const enoughPlayers = room.players.length >= setting.minumumPlayers
-    const readyForEnoughTime = room.allPlayerReadyTime >= setting.gameStartCountDown
-    if (enoughPlayers && allPlayerReady) {
-      if (readyForEnoughTime) {
-        room.allPlayerReadyTime = 0
-        changeGameStatus(room, 'processing')
-      } else {
-        room.allPlayerReadyTime += intervalTimeStep
-      }
-    } else {
-      room.allPlayerReadyTime = 0
-    }
-  }, intervalTimeStep
-)
-
-const createCoin = () => {
-  const mapFile = serverMap.processing.map
-  const mapUrl = `../share/map/${mapFile}`
-  const map = require(mapUrl)
-  const infoLayer = map.layers.find(layer => layer.name === 'info_layer')
-  const coinSpawnPoints = infoLayer.objects.filter(object => object.name === 'coin_point')
-  const randomCoinSpawnIndex = Math.floor(Math.random() * (coinSpawnPoints.length))
-  const coinSpawnPoint = coinSpawnPoints[randomCoinSpawnIndex]
-  const coinConstructor = {
-    interface: 'Item',
-    id: uuid(),
-    itemKey: 'coin',
-    position: { x: coinSpawnPoint.x, y: coinSpawnPoint.y },
-    velocity: { x: 0, y: 0 },
-    phaserObject: null
-  }
-  return coinConstructor
-}
-
-const registerProcessingIntervals = room => setInterval(
-  () => {
-    const coins = room.items.filter(item => item.itemKey === 'coin')
-    if (coins.length > 0) {
-      // do nothing
-    } else {
-      const coinConstructor = createCoin()
-      room.items.push(coinConstructor)
-      const { io } = require('./index.js')
-      io.in(room.id).emit('dungeon', 'addItem', coinConstructor)
-    }
-
-    // check end game condition
-    // -> emit game end event
-    // -> change game status to ending
-  }, intervalTimeStep
-)
-
-const registerEndingIntervals = room => setInterval(
-  () => {
-    // generate end game report
-    // -> emit end game report to clients
-    // -> setTimeout and cycle gameStatus to waiting
-  }, intervalTimeStep
-)
-
-const changeGameStatus = (room, newGameStatus) => {
-  const gameStatusIntervals = room.intervals.byGameStatus
-  gameStatusIntervals.forEach(interval => clearInterval(interval))
-  gameStatusIntervals.splice(0, gameStatusIntervals.length)
-
-  if (newGameStatus === 'waiting') {
-    gameStatusIntervals.push(registerWaitingIntervals(room))
-  } else if (newGameStatus === 'processing') {
-    gameStatusIntervals.push(registerProcessingIntervals(room))
-  } else if (newGameStatus === 'ending') {
-    gameStatusIntervals.push(registerEndingIntervals(room))
-  } else {
-    // wrong status, throw
-  }
-
-  room.gameStatus = newGameStatus
-  const { io } = require('./index.js')
-  io.to(room.id).emit('game', 'updateGameStatus', room.gameStatus)
 }
 
 const reconnectPlayer = (room, userId) => {
