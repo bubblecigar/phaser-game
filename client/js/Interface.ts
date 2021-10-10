a282645840f0f20579c243cb8efa40d33a9a5394
王瑞瑜 <roywang@h2o-team.com>
2021-10-10 23:17:32 +0800

    fix missing prop gameStatus of interface GameState

--- client/js/Interface.ts
+++ client/js/Interface.ts
@@ -61,5 +61,6 @@ export interface Bullet extends Omit<Item, 'interface'> {
 export interface GameState {
   players: Player[],
   items: Item[],
+  gameStatus: 'waiting' | 'processing' | 'ending',
   monsters: Monster[]
 }
