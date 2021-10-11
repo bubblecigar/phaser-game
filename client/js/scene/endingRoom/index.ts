6d5485ecebfa6f983826e2255d510c33c6b26132
王瑞瑜 <roywang@h2o-team.com>
2021-10-12 05:47:40 +0800

    add winnerStatue in endingRoom

--- client/js/scene/endingRoom/index.ts
+++ client/js/scene/endingRoom/index.ts
@@ -1,7 +1,42 @@
+import Phaser from 'phaser'
 import basescene from '../share/index'
+import gameState from '../../game/state'
+import charactors from '../../charactors/index'
+import setting from '../../../../share/setting.json'
 
 function create() {
   basescene.create.apply(this)
+
+  try {
+    const infoLayer = this.map.objects.find(layer => layer.name === 'info_layer')
+    const winnerPoint = infoLayer.objects.find(object => object.name === 'winner_point')
+    const winner = gameState.winner
+    const charactor = charactors[winner.charactorKey]
+    const spriteSheetKey = charactor.spritesheetConfig.spritesheetKey
+    const { size, origin } = charactor.matterConfig
+
+    const winnerSprite = this.add.sprite(winnerPoint.x, winnerPoint.y, spriteSheetKey)
+    winnerSprite.setOrigin(origin.x, origin.y)
+    winnerSprite.setTint(0xFFD700)
+    winnerSprite.setScale(2, 2)
+
+    const Bodies = Phaser.Physics.Matter.Matter.Bodies
+    const rect = Bodies.rectangle(winnerPoint.x, winnerPoint.y, 2 * size.width, 2 * size.height, { label: 'player-body' })
+    const winnerMatter = this.matter.add.gameObject(winnerSprite)
+    winnerMatter.setExistingBody(rect)
+
+
+    const winnerName = this.add.text(winnerPoint.x, winnerPoint.y - 20, winner.name, {
+      fontSize: setting.fontSize
+    })
+    winnerName.setOrigin(0.5, 0.5)
+    const title = this.add.text(winnerPoint.x, winnerPoint.y - 40, "winner", {
+      fontSize: setting.fontSize
+    })
+    title.setOrigin(0.5, 0.5)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function update(...args) {
