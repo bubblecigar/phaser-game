4b41dcf3d5758cd1f39eb1a1a637ac78a00d2fb2
王瑞瑜 <roywang@h2o-team.com>
2021-10-12 22:48:20 +0800

    refactor createWinnerStatue() from create() of endingScene

--- client/js/scene/endingRoom/index.ts
+++ client/js/scene/endingRoom/index.ts
@@ -4,36 +4,40 @@ import gameState from '../../game/state'
 import charactors from '../../charactors/index'
 import setting from '../../../../share/setting.json'
 
+const createWinnerStatue = (scene) => {
+  const infoLayer = scene.map.objects.find(layer => layer.name === 'info_layer')
+  const winnerPoint = infoLayer.objects.find(object => object.name === 'winner_point')
+  const winner = gameState.winner
+  const charactor = charactors[winner.charactorKey]
+  const spriteSheetKey = charactor.spritesheetConfig.spritesheetKey
+  const { size, origin } = charactor.matterConfig
+
+  const winnerSprite = scene.add.sprite(winnerPoint.x, winnerPoint.y, spriteSheetKey)
+  winnerSprite.setOrigin(origin.x, origin.y)
+  winnerSprite.setTint(0xFFD700)
+  winnerSprite.setScale(2, 2)
+
+  const Bodies = Phaser.Physics.Matter.Matter.Bodies
+  const rect = Bodies.rectangle(winnerPoint.x, winnerPoint.y, 2 * size.width, 2 * size.height, { label: 'player-body' })
+  const winnerMatter = scene.matter.add.gameObject(winnerSprite)
+  winnerMatter.setExistingBody(rect)
+
+
+  const winnerName = scene.add.text(winnerPoint.x, winnerPoint.y - 20, winner.name, {
+    fontSize: setting.fontSize
+  })
+  winnerName.setOrigin(0.5, 0.5)
+  const title = scene.add.text(winnerPoint.x, winnerPoint.y - 40, "winner", {
+    fontSize: setting.fontSize
+  })
+  title.setOrigin(0.5, 0.5)
+}
+
 function create() {
   basescene.create.apply(this)
 
   try {
-    const infoLayer = this.map.objects.find(layer => layer.name === 'info_layer')
-    const winnerPoint = infoLayer.objects.find(object => object.name === 'winner_point')
-    const winner = gameState.winner
-    const charactor = charactors[winner.charactorKey]
-    const spriteSheetKey = charactor.spritesheetConfig.spritesheetKey
-    const { size, origin } = charactor.matterConfig
-
-    const winnerSprite = this.add.sprite(winnerPoint.x, winnerPoint.y, spriteSheetKey)
-    winnerSprite.setOrigin(origin.x, origin.y)
-    winnerSprite.setTint(0xFFD700)
-    winnerSprite.setScale(2, 2)
-
-    const Bodies = Phaser.Physics.Matter.Matter.Bodies
-    const rect = Bodies.rectangle(winnerPoint.x, winnerPoint.y, 2 * size.width, 2 * size.height, { label: 'player-body' })
-    const winnerMatter = this.matter.add.gameObject(winnerSprite)
-    winnerMatter.setExistingBody(rect)
-
-
-    const winnerName = this.add.text(winnerPoint.x, winnerPoint.y - 20, winner.name, {
-      fontSize: setting.fontSize
-    })
-    winnerName.setOrigin(0.5, 0.5)
-    const title = this.add.text(winnerPoint.x, winnerPoint.y - 40, "winner", {
-      fontSize: setting.fontSize
-    })
-    title.setOrigin(0.5, 0.5)
+    createWinnerStatue(this)
   } catch (error) {
     console.log(error)
   }
