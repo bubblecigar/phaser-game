a72acfa628cae29e052f8498f6c6c0f1b0c5cb8b
王瑞瑜 <roywang@h2o-team.com>
2021-10-15 14:34:40 +0800

    add scene/collisionCategories.ts

---
+++ client/js/scene/basescene/collisionCategories.ts
@@ -0,0 +1,10 @@
+// Tile map block's default category is one, so we don't need to set it mannually
+
+export default {
+  CATEGORY_MAP_BLOCK: 1,
+  CATEGORY_PLAYER: 2,
+  CATEGORY_ENEMY: 4,
+  CATEGORY_PLAYER_BULLET: 8,
+  CATEGORY_ENEMY_BULLET: 16,
+  CATEGORY_TRANSPARENT: 32
+}
