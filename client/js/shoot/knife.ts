9d51cfb01ffa0e268be5cfaf71ff416ac55a258a
王瑞瑜 <roywang@h2o-team.com>
2021-10-15 12:56:55 +0800

    remove unused functions

--- client/js/shoot/knife.ts
+++ client/js/shoot/knife.ts
@@ -1,31 +1,6 @@
 import { v4 } from 'uuid'
 import bulletsRef from './ref'
 
-const createArrowHead = (scene, position) => {
-  const Bodies = Phaser.Physics.Matter.Matter.Bodies
-  const headBody = Bodies.rectangle(position.x, position.y - 6, 2, 2)
-  const headMatter = scene.matter.add.sprite(position.x, position.y, 'arrow_sprite')
-  headMatter.setExistingBody(headBody)
-  headMatter.setOrigin(0.5, 0.1)
-  headMatter.setFriction(1, 0, 0)
-  headMatter.setMass(0.01)
-
-  return headMatter
-}
-
-const createArrowFeather = (scene, position) => {
-  const Bodies = Phaser.Physics.Matter.Matter.Bodies
-  const feather = scene.add.rectangle(position.x, position.y + 6, 2, 2)
-  const featherBody = Bodies.rectangle(position.x, position.y + 6, 2, 2)
-  const featherMatter = scene.matter.add.gameObject(feather)
-  featherMatter.setExistingBody(featherBody)
-  featherMatter.setMass(0.001)
-  featherMatter.setFriction(0, 0.4, 0)
-  featherMatter.setCollisionGroup(-1)
-
-  return featherMatter
-}
-
 export const shootKnife = ({ scene, from, to, builderId }) => {
   const velocity = 5
   const angle = Math.atan2(to.y - from.y, to.x - from.x)
