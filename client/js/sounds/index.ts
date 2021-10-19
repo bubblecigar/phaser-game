ae9db2df5fa8beb0087a1bb348abe6ea59c294cb
王瑞瑜 <roywang@h2o-team.com>
2021-10-19 14:44:52 +0800

    minify sound files from  .wav to .mp3

--- client/js/sounds/index.ts
+++ client/js/sounds/index.ts
@@ -1,7 +1,7 @@
-import small_hit from '../../statics/sound/small_hit.wav'
-import tap from '../../statics/sound/tap.wav'
-import bot from '../../statics/sound/bot.wav'
-import background from '../../statics/sound/8-bit-game-music_by_turpak_preview.wav'
+import small_hit from '../../statics/sound/small_hit.mp3'
+import tap from '../../statics/sound/tap.mp3'
+import bot from '../../statics/sound/bot.mp3'
+import background from '../../statics/sound/8-bit-game-music_by_turpak_preview.mp3'
 export const sounds = {
   quickJump: {
     url: small_hit,
