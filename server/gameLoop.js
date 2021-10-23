e25242ae04de3713d90d6760e7d3e0afbf276ee4
王瑞瑜 <roywang@h2o-team.com>
2021-10-23 15:55:12 +0800

    randomize monsterSpawnPoint

--- server/gameLoop.js
+++ server/gameLoop.js
@@ -67,7 +67,8 @@ const createMonster = () => {
   const map = require(mapUrl)
   const infoLayer = map.layers.find(layer => layer.name === 'info_layer')
   const monsterSpawnPoints = infoLayer.objects.filter(object => object.name === 'monster_point')
-  const monsterSpawnPoint = monsterSpawnPoints[0]
+  const randomMonsterSpawnIndex = Math.floor(Math.random() * (monsterSpawnPoints.length))
+  const monsterSpawnPoint = monsterSpawnPoints[randomMonsterSpawnIndex]
   const monsterConstructor = {
     interface: 'Monster',
     id: uuid(),
