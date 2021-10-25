7b1955bb62fef098156b44fbc66a343bc2e142b2
王瑞瑜 <roywang@h2o-team.com>
2021-10-25 17:10:48 +0800

    add neutrals.json to contral monster behavior

--- server/monster.js
+++ server/monster.js
@@ -1,6 +1,7 @@
 const uuid = require('uuid')
 const setting = require('../share/setting.json')
 const serverMap = require('../share/serverMap.json')
+const neutrals = require('./neutrals.json')
 
 const createMonster = () => {
   const mapFile = serverMap.processing.map
@@ -10,13 +11,16 @@ const createMonster = () => {
   const monsterSpawnPoints = infoLayer.objects.filter(object => object.name === 'monster_point')
   const randomMonsterSpawnIndex = Math.floor(Math.random() * (monsterSpawnPoints.length))
   const monsterSpawnPoint = monsterSpawnPoints[randomMonsterSpawnIndex]
+  const monsterKeys = Object.keys(neutrals)
+  const randomMonsterKey = monsterKeys[Math.floor(Math.random() * (monsterKeys.length))]
+  const randomMonster = neutrals[randomMonsterKey]
   const monsterConstructor = {
     interface: 'Monster',
     id: uuid(),
-    health: 100,
     properties: monsterSpawnPoint.properties,
     builderId: 'server',
-    charactorKey: 'orge',
+    charactorKey: randomMonster.key,
+    health: randomMonster.health,
     drop: 'coin',
     position: { x: monsterSpawnPoint.x, y: monsterSpawnPoint.y },
     velocity: { x: 0, y: 0 }
@@ -28,22 +32,21 @@ const runMonsterScript = (room, monster) => {
   const shoot = () => {
     const monsterAlive = room.monsters.find(m => m.id === monster.id)
     if (!monsterAlive) { return }
-    const shootType = 'soundwave'
     const shootOption = {
       from: monster.position,
       to: { x: monster.position.x + 10, y: monster.position.y },
       builderId: monster.id,
-      type: shootType,
+      type: neutrals[monster.charactorKey].shootType,
       options: {
-        type: shootType,
+        type: neutrals[monster.charactorKey].shootType,
         randomNumber: Math.random()
       }
     }
     const { io } = require('./index.js')
     io.in(room.id).emit('dungeon', 'shoot', shootOption)
-    setTimeout(() => shoot(), 3000)
+    setTimeout(() => shoot(), neutrals[monster.charactorKey].shootInterval)
   }
-  setTimeout(() => shoot(), 3000)
+  setTimeout(() => shoot(), neutrals[monster.charactorKey].shootInterval)
 
   const dx = monster.properties ? monster.properties[0].value : 0
   const centralPosition = { x: monster.position.x, y: monster.position.y }
