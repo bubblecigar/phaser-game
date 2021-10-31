7defc364e0b96925b6d8edc03863f538cb7dae84
王瑞瑜 <roywang@h2o-team.com>
2021-10-31 20:30:53 +0800

    switch name of throws and shoot

--- client/js/actions/shoot.ts
+++ client/js/actions/shoot.ts
@@ -1,37 +1,6 @@
 import { v4 } from 'uuid'
-import collisionCategories from './collisionCategories'
-import { playAnimation, normalizeMatter } from './utils'
 import items from '../items/index'
-
-const createArrowHead = (scene, position, collisionCategory, collisionTargets, performer, options) => {
-  const Bodies = Phaser.Physics.Matter.Matter.Bodies
-  const headBody = Bodies.rectangle(position.x, position.y - 6, 2, 2)
-  const item = items[options.item] || items["arrow"]
-  const headMatter = scene.matter.add.sprite(position.x, position.y, item.spritesheetConfig.spritesheetKey)
-  normalizeMatter(performer, item, headMatter)
-  playAnimation(item, headMatter)
-  headMatter.setExistingBody(headBody)
-  headMatter.setOrigin(0.5, 0.1)
-  headMatter.setFriction(1, 0, 0)
-  headMatter.setMass(0.05)
-  headMatter.setCollisionCategory(collisionCategory)
-  headMatter.setCollidesWith(collisionTargets)
-
-  return headMatter
-}
-
-const createArrowFeather = (scene, position) => {
-  const Bodies = Phaser.Physics.Matter.Matter.Bodies
-  const feather = scene.add.rectangle(position.x, position.y + 6, 2, 2)
-  const featherBody = Bodies.rectangle(position.x, position.y + 6, 2, 2)
-  const featherMatter = scene.matter.add.gameObject(feather)
-  featherMatter.setExistingBody(featherBody)
-  featherMatter.setMass(0.001)
-  featherMatter.setFriction(0, 0.2, 0)
-  featherMatter.setCollisionCategory(collisionCategories.CATEGORY_TRANSPARENT)
-
-  return featherMatter
-}
+import { normalizeMatter, playAnimation } from './utils'
 
 export const shoot = ({
   scene,
@@ -42,43 +11,56 @@ export const shoot = ({
   collisionTargets,
   options
 }) => {
-  const velocity = 7
   const from = performer.position
   const to = target
   const angle = Math.atan2(to.y - from.y, to.x - from.x)
 
   const id = v4()
-  const headMatter = createArrowHead(scene, from, collisionCategory, collisionTargets, performer, options)
-  const featherMatter = createArrowFeather(scene, from)
-  const constraint = scene.matter.add.constraint(headMatter.body, featherMatter.body, 12)
-  headMatter.setVelocityX(velocity * Math.cos(angle))
-  headMatter.setVelocityY(velocity * Math.sin(angle))
+  const velocity = 4
+
+  const item = items[options.item] || items["fireball"]
+
+  const matter = scene.matter.add.sprite(from.x, from.y, item.spritesheetConfig.spritesheetKey)
+
+  normalizeMatter(performer, item, matter)
+
+  matter.setAngle((angle * 180 / Math.PI) + 90)
+  matter.setVelocityX(velocity * Math.cos(angle))
+  matter.setVelocityY(velocity * Math.sin(angle))
+  matter.setFixedRotation(true)
+  matter.setIgnoreGravity(true)
+  matter.setFriction(1, 0, 1)
+
+  playAnimation(item, matter)
+
+  matter.setCollisionCategory(collisionCategory)
+  matter.setCollidesWith(collisionTargets)
 
   const update = () => {
-    const { x: x1, y: y1 } = headMatter
-    const { x: x2, y: y2 } = featherMatter
-    const angleDeg = Math.atan2(y1 - y2, x1 - x2) * 180 / Math.PI
-    headMatter.setAngle(angleDeg + 90)
+    const velocity = matter.body.velocity
+    if (Math.abs(velocity.x) + Math.abs(velocity.y) <= 1) {
+      destroy()
+    }
   }
+
   const destroy = () => {
     if (itemStorage[id]) {
       delete itemStorage[id]
-      scene.matter.world.remove(constraint)
-      headMatter.destroy()
-      featherMatter.destroy()
+      matter.destroy()
     }
   }
-  headMatter.setData({
+
+  matter.setData({
     id,
     interface: 'Bullet',
     builderId: performer.id,
-    damage: 8,
-    phaserObject: headMatter,
+    damage: 12,
+    phaserObject: matter,
     destroy
   })
 
   scene.time.delayedCall(
-    1000,
+    5000,
     () => destroy(),
     null,
     scene
--- client/js/actions/throws.ts
+++ client/js/actions/throws.ts
@@ -1,6 +1,37 @@
 import { v4 } from 'uuid'
+import collisionCategories from './collisionCategories'
+import { playAnimation, normalizeMatter } from './utils'
 import items from '../items/index'
-import { normalizeMatter, playAnimation } from './utils'
+
+const createArrowHead = (scene, position, collisionCategory, collisionTargets, performer, options) => {
+  const Bodies = Phaser.Physics.Matter.Matter.Bodies
+  const headBody = Bodies.rectangle(position.x, position.y - 6, 2, 2)
+  const item = items[options.item] || items["arrow"]
+  const headMatter = scene.matter.add.sprite(position.x, position.y, item.spritesheetConfig.spritesheetKey)
+  normalizeMatter(performer, item, headMatter)
+  playAnimation(item, headMatter)
+  headMatter.setExistingBody(headBody)
+  headMatter.setOrigin(0.5, 0.1)
+  headMatter.setFriction(1, 0, 0)
+  headMatter.setMass(0.05)
+  headMatter.setCollisionCategory(collisionCategory)
+  headMatter.setCollidesWith(collisionTargets)
+
+  return headMatter
+}
+
+const createArrowFeather = (scene, position) => {
+  const Bodies = Phaser.Physics.Matter.Matter.Bodies
+  const feather = scene.add.rectangle(position.x, position.y + 6, 2, 2)
+  const featherBody = Bodies.rectangle(position.x, position.y + 6, 2, 2)
+  const featherMatter = scene.matter.add.gameObject(feather)
+  featherMatter.setExistingBody(featherBody)
+  featherMatter.setMass(0.001)
+  featherMatter.setFriction(0, 0.2, 0)
+  featherMatter.setCollisionCategory(collisionCategories.CATEGORY_TRANSPARENT)
+
+  return featherMatter
+}
 
 export const throws = ({
   scene,
@@ -11,56 +42,43 @@ export const throws = ({
   collisionTargets,
   options
 }) => {
+  const velocity = 7
   const from = performer.position
   const to = target
   const angle = Math.atan2(to.y - from.y, to.x - from.x)
 
   const id = v4()
-  const velocity = 4
-
-  const item = items[options.item] || items["fireball"]
-
-  const matter = scene.matter.add.sprite(from.x, from.y, item.spritesheetConfig.spritesheetKey)
-
-  normalizeMatter(performer, item, matter)
-
-  matter.setAngle((angle * 180 / Math.PI) + 90)
-  matter.setVelocityX(velocity * Math.cos(angle))
-  matter.setVelocityY(velocity * Math.sin(angle))
-  matter.setFixedRotation(true)
-  matter.setIgnoreGravity(true)
-  matter.setFriction(1, 0, 1)
-
-  playAnimation(item, matter)
-
-  matter.setCollisionCategory(collisionCategory)
-  matter.setCollidesWith(collisionTargets)
+  const headMatter = createArrowHead(scene, from, collisionCategory, collisionTargets, performer, options)
+  const featherMatter = createArrowFeather(scene, from)
+  const constraint = scene.matter.add.constraint(headMatter.body, featherMatter.body, 12)
+  headMatter.setVelocityX(velocity * Math.cos(angle))
+  headMatter.setVelocityY(velocity * Math.sin(angle))
 
   const update = () => {
-    const velocity = matter.body.velocity
-    if (Math.abs(velocity.x) + Math.abs(velocity.y) <= 1) {
-      destroy()
-    }
+    const { x: x1, y: y1 } = headMatter
+    const { x: x2, y: y2 } = featherMatter
+    const angleDeg = Math.atan2(y1 - y2, x1 - x2) * 180 / Math.PI
+    headMatter.setAngle(angleDeg + 90)
   }
-
   const destroy = () => {
     if (itemStorage[id]) {
       delete itemStorage[id]
-      matter.destroy()
+      scene.matter.world.remove(constraint)
+      headMatter.destroy()
+      featherMatter.destroy()
     }
   }
-
-  matter.setData({
+  headMatter.setData({
     id,
     interface: 'Bullet',
     builderId: performer.id,
-    damage: 12,
-    phaserObject: matter,
+    damage: 8,
+    phaserObject: headMatter,
     destroy
   })
 
   scene.time.delayedCall(
-    5000,
+    1000,
     () => destroy(),
     null,
     scene
