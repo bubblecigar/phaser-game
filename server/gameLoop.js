2c8c965fef5d9a6f0eee4302e5d27f87b3a71f7e
王瑞瑜 <roywang@h2o-team.com>
2021-11-02 17:50:51 +0800

    fix missing inti value for reducer

--- server/gameLoop.js
+++ server/gameLoop.js
@@ -27,8 +27,8 @@ const registerWaitingIntervals = room => setInterval(
     const allPlayerReady = !room.players.some(player => !player.ready)
     const enoughPlayers = room.players.length >= setting.minumumPlayers
     const readyForEnoughTime = room.allPlayerReadyTime >= setting.gameStartCountDown
-    const redTeamMemberCount = room.players.reduce((acc, cur) => acc + cur.team === 'red' ? 1 : 0)
-    const blueTeamMemberCount = room.players.reduce((acc, cur) => acc + cur.team === 'blue' ? 1 : 0)
+    const redTeamMemberCount = room.players.reduce((acc, cur) => acc + cur.team === 'red' ? 1 : 0, 0)
+    const blueTeamMemberCount = room.players.reduce((acc, cur) => acc + cur.team === 'blue' ? 1 : 0, 0)
     if (enoughPlayers && allPlayerReady && redTeamMemberCount === blueTeamMemberCount) {
       if (readyForEnoughTime) {
         room.allPlayerReadyTime = 0
